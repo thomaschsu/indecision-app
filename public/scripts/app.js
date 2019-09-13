cdc5e0a5f56be0bedfe3ac0bd2945e4a097b79e4
Tom Hsu <thomaschsu@gmail.com>
2019-09-13 11:34:30 -0400

    Modified jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,36 @@
-console.log('App.js is running')
+'use strict';
+
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>
-var template = React.createElement("h1", {
-    id: "someid"
-}, "This is JSX from app.js!");
-var appRoot = document.getElementById('app')
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-console.log('App.js is running')
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js!</p>
-var appRoot = document.getElementById('app')
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot)
