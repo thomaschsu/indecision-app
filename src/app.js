21f79d75284ef8e2c6eaf9c4289f865e072dd9ac
Tom Hsu <thomaschsu@gmail.com>
2019-09-29 20:34:05 -0400

    Added default props

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this)
         this.handleAddOption = this.handleAddOption.bind(this)
         this.state = {
-            options: []
+            options: props.options
         }
     }
     handleDeleteOptions() {
@@ -35,12 +35,11 @@ class IndecisionApp extends React.Component {
         })
     }
     render() {
-        const title = 'Indecision'
         const subtitle = 'Put your life in the hands of a computer'
 
         return (
             <div>
-                <Header title={title} subtitle={subtitle} />
+                <Header subtitle={subtitle} />
                 <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
                 <Options
                     options={this.state.options}
@@ -54,13 +53,21 @@ class IndecisionApp extends React.Component {
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+}
+
 const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     )
+}
+
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
 const Action = (props) => {
@@ -123,4 +130,4 @@ class AddOption extends React.Component {
     }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp options={['Devils den', 'Second District']}/>, document.getElementById('app'))
