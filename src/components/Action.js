3d8c30df26d7640fae86826fc711107e64251fb7
Tom Hsu <thomaschsu@gmail.com>
2019-10-11 13:19:28 -0400

    Setup media queries

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Action = (props) => (
     <div>
-        <button onClick={props.handlePick}
+        <button className="big-button" onClick={props.handlePick}
             disabled={!props.hasOptions}
         >
             What should I do?
